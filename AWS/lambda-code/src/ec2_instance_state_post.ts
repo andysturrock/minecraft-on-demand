073d37c577241fd03de77a8f0f887694543aade4
Andy Sturrock <github@sturrock.org>
2022-08-11 18:44:27 +0100

    Refactor and sort out debug messages to improve readability.  Still using debug cron pattern.

--- AWS/lambda-code/src/ec2_instance_state_post.ts
+++ AWS/lambda-code/src/ec2_instance_state_post.ts
@@ -1,70 +1,35 @@
 import util from 'util';
 import { Context, APIGatewayEvent } from 'aws-lambda';
-import { EC2 } from '@aws-sdk/client-ec2';
-import { EventBridge, PutRuleCommandInput, PutTargetsCommandInput, Target } from "@aws-sdk/client-eventbridge";
+import { EC2, StartInstancesCommandInput, StartVpcEndpointServicePrivateDnsVerificationCommand, StopInstancesCommandInput } from '@aws-sdk/client-ec2';
+import { DeleteRuleCommandInput, EventBridge, PutRuleCommandInput, PutTargetsCommandInput, RemoveTargetsCommandInput, Target } from "@aws-sdk/client-eventbridge";
 
 async function lambdaHandler(event: APIGatewayEvent, context: Context): Promise<any> {
   try {
-    console.log(`event: ${util.inspect(event)}`);
-    console.log(`context: ${util.inspect(context)}`);
+    console.debug(`event: ${util.inspect(event)}`);
+    console.debug(`context: ${util.inspect(context)}`);
     const body = JSON.parse(event.body!);
     const action = body.action;
-    const instanceId = body?.InstanceId;
+    if(action === undefined) {
+      throw new Error("Missing action field in body");
+    }
+    const instanceId:string = body?.instanceId;
+    if(instanceId === undefined) {
+      throw new Error("Missing instanceId field in body");
+    }
+    const invokedFunctionArn = context.invokedFunctionArn;
     
     // TODO pass in region
     const region = {region: 'eu-west-2'};
     const ec2Client = new EC2(region);
-    
-    var ec2Params = {
-      InstanceIds: [instanceId],
-      DryRun: false
-    };
+    const eventBridgeClient = new EventBridge(region);
     
     if(action === "start") {
-      console.log("Creating eventbridge rule to stop server...");
-      const eventBridgeClient = new EventBridge(region);
-      const now = new Date();
-      const hours = now.getUTCHours() + 2;
-      const minutes = now.getUTCMinutes();
-      // const cronPattern = `cron(${minutes} ${hours} * * ? *)`;
-      const cronPattern = `cron(* * * * ? *)`;
-      const putRuleParams: PutRuleCommandInput = {
-        Name: `Turn_off_${instanceId}`,
-        ScheduleExpression: cronPattern
-      };
-      console.log(`putRuleParams = ${JSON.stringify(putRuleParams)}`);
-      const putRuleResult = await eventBridgeClient.putRule(putRuleParams);
-      console.log(`putRule res = ${JSON.stringify(putRuleResult)}`);
-      const body = {
-        "action": "stop",
-        "InstanceId": instanceId
-      };
-      const input = {
-        body: JSON.stringify(body)
-      }
-      const invokedFunctionArn = context.invokedFunctionArn;
-      let target: Target = {
-        Id: `Turn_off_${instanceId}`,
-        Arn: invokedFunctionArn,
-        Input: JSON.stringify(input)
-      };
-      console.log(`putRule target = ${JSON.stringify(target)}`);
-      const putTargetsCommandInput: PutTargetsCommandInput = {
-        Rule: `Turn_off_${instanceId}`,
-        Targets: [target]
-      };
-      const putTargetResult = await eventBridgeClient.putTargets(putTargetsCommandInput);
-      console.log(`putRule putTargetResult = ${JSON.stringify(putTargetResult)}`);
-      
-      console.log("Starting server...");
-      // const data = await ec2Client.startInstances(ec2Params);
-      // console.log(`start data: ${util.inspect(data)}`);
+      await startServer(instanceId, invokedFunctionArn, eventBridgeClient, ec2Client);
     } else if(action === "stop") {
-      console.log("Stopping server...");
-      const data = await ec2Client.stopInstances(ec2Params);
-      console.log(`stop data: ${util.inspect(data)}`);
+      const deleteStopRule = body.deleteStopRule;
+      await stopServer(instanceId, deleteStopRule, eventBridgeClient, ec2Client);
     } else {
-      console.log(`Unexpected action: ${action}`);
+      console.warn(`Unexpected action: ${action}`);
     }
     return {
       statusCode: 200,
@@ -72,30 +37,103 @@ async function lambdaHandler(event: APIGatewayEvent, context: Context): Promise<
     }
   }
   catch (err) {
+    let errorText = "Unknown error";
     if(err instanceof Error) {
-      console.error(`Error: ${err.stack}`);
+      console.error(`Caught Error exception: ${err.stack}`);
+      const error = err as Error;
+      errorText = error.message;
     } else {
-      console.error(`Error: ${JSON.stringify(err)}`);
+      console.error(`Caught exception: ${JSON.stringify(err)}`);
     }
     return {
       statusCode: 500,
-      body: "Error"
+      body: JSON.stringify({
+        error: errorText
+      })
     }
   }
 }
 
 export { lambdaHandler };
 
-/*
-{
-  "event": {
-    "body": {
-      "action": "start",
-      "InstanceId": "i-03a206101969e1f87"
-    }
-  },
-  "context": {
-    "invokedFunctionArn": "1223"
+
+async function startServer(instanceId: string,
+    invokedFunctionArn: string,
+    eventBridgeClient: EventBridge,
+    ec2Client: EC2) {
+  console.log("Creating EventBridge rule to stop server...");
+  const now = new Date();
+  const hours = now.getUTCHours();
+  const minutes = now.getUTCMinutes() + 1;
+  const day = now.getUTCDate();
+  const month = now.getUTCMonth() + 1; // Date uses 0-11, cron uses 1-12
+  const year = now.getUTCFullYear();
+  // TODO remove debug cron above
+  const cronPattern = `cron(${minutes} ${hours} ${day} ${month} ? ${year})`;
+  const putRuleParams: PutRuleCommandInput = {
+    Name: `Turn_off_${instanceId}`,
+    ScheduleExpression: cronPattern
+  };
+  console.debug(`putRuleParams = ${JSON.stringify(putRuleParams)}`);
+  const putRuleResult = await eventBridgeClient.putRule(putRuleParams);
+  console.debug(`putRuleResult = ${JSON.stringify(putRuleResult)}`);
+  const body = {
+    action: "stop",
+    instanceId: instanceId,
+    deleteStopRule: true
+  };
+  const input = {
+    body: JSON.stringify(body)
   }
+  let target: Target = {
+    Id: `Turn_off_${instanceId}`,
+    Arn: invokedFunctionArn,
+    Input: JSON.stringify(input)
+  };
+  const putTargetsCommandInput: PutTargetsCommandInput = {
+    Rule: `Turn_off_${instanceId}`,
+    Targets: [target]
+  };
+  console.debug(`putTargetsCommandInput = ${JSON.stringify(putTargetsCommandInput)}`);
+  const putTargetResult = await eventBridgeClient.putTargets(putTargetsCommandInput);
+  console.debug(`putTargetResult = ${JSON.stringify(putTargetResult)}`);
+  
+  console.log("Starting server...");
+  const startInstancesCommandInput: StartInstancesCommandInput = {
+    InstanceIds: [instanceId],
+  };
+  console.debug(`startInstancesCommandInput: ${JSON.stringify(startInstancesCommandInput)}`);
+  const startInstancesResult = await ec2Client.startInstances(startInstancesCommandInput);
+  console.debug(`startInstancesResult: ${JSON.stringify(startInstancesResult)}`);
 }
-*/
+
+async function stopServer(instanceId: string,
+    deleteStopRule: boolean,
+    eventBridgeClient: EventBridge,
+    ec2Client: EC2) {
+  if(deleteStopRule) {
+    console.log(`Deleting EventBridge stop server rule...`);
+    const removeTargetsCommandInput: RemoveTargetsCommandInput = {
+      Rule: `Turn_off_${instanceId}`,
+      Ids: [`Turn_off_${instanceId}`]
+    };
+    console.debug(`removeTargetsCommandInput = ${JSON.stringify(removeTargetsCommandInput)}`);
+    const removeTargetsResult = await eventBridgeClient.removeTargets(removeTargetsCommandInput)
+    console.debug(`removeTargetsResult = ${JSON.stringify(removeTargetsResult)}`);
+
+    const deleteRuleCommandInput: DeleteRuleCommandInput = {
+      Name: `Turn_off_${instanceId}`
+    }
+    console.debug(`deleteRuleCommandInput = ${JSON.stringify(deleteRuleCommandInput)}`);
+    const deleteRuleResult = await eventBridgeClient.deleteRule(deleteRuleCommandInput);
+    console.debug(`deleteRuleResult = ${JSON.stringify(deleteRuleResult)}`);
+  }
+
+  console.log("Stopping server...");
+  const stopInstancesCommandInput: StopInstancesCommandInput = {
+    InstanceIds: [instanceId],
+  };
+  console.debug(`stopInstancesCommandInput = ${JSON.stringify(stopInstancesCommandInput)}`);
+  const stopInstancesResult = await ec2Client.stopInstances(stopInstancesCommandInput);
+  console.debug(`stopInstancesResult = ${JSON.stringify(stopInstancesResult)}`);
+}
