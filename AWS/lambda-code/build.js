a05cd61ba6a1046e885c3a9ea70a538a1b2b757d
Andy Sturrock <github@sturrock.org>
2022-09-26 20:16:32 +0100

    Fix lint errors

--- AWS/lambda-code/build.js
+++ AWS/lambda-code/build.js
@@ -1,8 +1,11 @@
 'use strict';
 
+// Disable some eslint warnings because it gets confused in a mixed TS/JS project.
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const glob = require('glob-promise');
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const AdmZip = require("adm-zip");
 
 async function main() {
@@ -11,11 +14,11 @@ async function main() {
   const zipfilePath = path.join(distDirPath, 'lambda.zip');
   const jsFiles = await glob('dist/*.js');
   jsFiles.forEach(jsFile => {
-    console.log(`adding ${jsFile}`)
+    console.log(`adding ${jsFile}`);
     zip.addLocalFile(jsFile);
-  })
+  });
   const node_modules = path.join('.', 'node_modules');
-  console.log(`adding ${node_modules}`)
+  console.log(`adding ${node_modules}`);
   zip.addLocalFolder(node_modules, "node_modules");
   zip.writeZip(zipfilePath);
   console.log(`Created ${zipfilePath} successfully`);
